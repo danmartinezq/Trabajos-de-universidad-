8d5c24001d947a48f0fe77f3a34615e69819f211
danmartinezq <danielmartinezquintana026@gmail.com>
2026-08-26 10:46:28 -0300

    Nuevo trabajo

---
+++ clase_1.cpp
@@ -0,0 +1,21 @@
+#include <iostream> // Llamar la libreria
+
+using namespace std; // uso ee esoacio estandar
+
+int main(){
+
+    string nombre = "Daniel";
+    int edad = 18;
+    float altura = 1.60 ;
+    double Pi = 3.1415 ;
+    char inicial = 'D';
+    bool estudiante = false ;
+
+    cout << "Nombre del estudiante:" <<nombre <<endl ;
+    cout << "Edad del estuduante:" <<edad <<endl;
+    cout << "Estatura del estudiante:" <<altura <<endl;
+    cout << "valor de Pi:" <<Pi <<endl;
+    cout << "Es estudiante?" <<endl <<estudiante << endl;
+
+    return 0;
+} 
