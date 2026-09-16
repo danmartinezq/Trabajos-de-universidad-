4a53fcdef527397efb85127f9a19d1138950e498
danmartinezq <danielmartinezquintana026@gmail.com>
2026-09-16 16:35:29 -0300

    nuevos cambios

--- Do_WhileMenu.cpp
+++ Do_WhileMenu.cpp
@@ -3,23 +3,24 @@ using namespace std;
 
 int main(){
     int option;
-    cout<<"___Menu___"<<endl;
-    cout<<"1.Saludar."<<endl;
-    cout<<"2. Mostrar un mensaje."<<endl;
-    cout<<"3. Salir."<<endl;
+    do {
 
-    cout<<"Seleccionar una opción: ";
-    cin>>option;
-    if (option==1) {
-        cout<<"Hola usuario"<<endl;
-    
-    }else if (option==2) {
-        cout<<"Ayuda no puedo mas :( "<<endl;
-    
-    }else if (option==3) {
-        cout<<"Saliendo..."<<endl;
-    }else {
-        cout<<"Opcion no valida intente otra vez";
+        cout<<"___Menu___"<<endl;
+        cout<<"1.Saludar."<<endl;
+        cout<<"2. Mostrar un mensaje."<<endl;
+        cout<<"3. Salir."<<endl;
+
+        cout<<"Seleccionar una opción: ";
+        cin>>option;
+        if (option==1) {
+            cout<<"Hola usuario"<<endl;
+        }else if (option==2) {
+            cout<<"Ayuda no puedo mas :( "<<endl;
+        }else if (option==3) {
+            cout<<"Saliendo..."<<endl;
+        }else {
+            cout<<"Opcion no valida intente otra vez";
+        }
     }while (option!=3);
 
     return 0;
